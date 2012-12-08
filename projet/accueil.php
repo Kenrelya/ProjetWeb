98b0206139780cd2897e4996d0491a6b1fa1e246
ldestin <ldestin@172.16.250.81>
2012-12-08 22:19:28 +0100

    Test

--- projet/accueil.php
+++ projet/accueil.php
@@ -3,6 +3,7 @@
   $page_title="Accueil";
   include ("conn.php");
   ?>
+machin
 <div id="accueilDiv" class="class">
   <a href="enregistrement.php">S'enregistrer</a><br/>
   <a href="connexion.php">Se connecter</a>
