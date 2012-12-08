b258ec50a4420114c307f97dbee126c8e084edc6
sophia <sophia@sophia-Satellite-C660>
2012-12-08 22:29:16 +0100

    Test

--- projet/ajoutetu.php
+++ projet/ajoutetu.php
@@ -1,15 +1,10 @@
 <form action="ajoutetu2.php" method="post">
-Nom:
-<input type="text" name="nom"><br/><br/>
-Prénom:
-<input type="text" name="prenom"><br/><br/>
-Numero etudiant : 
-<input type="text" name="noet"><br/><br/>
-Annee scolaire :
-<input type="text" name="annee"><br/><br/>
-Filiere
-<input type="text" name="filiere"><br/><br/>
-
-<input type="submit" value="Ajouter">
+    <table>
+        <tr><td>Nom:</td><td><input type="text" name="nom"></td></tr>
+        <tr><td>Prénom:</td><td><input type="text" name="prenom"></td></tr>
+        <tr><td>Numero etudiant:</td><td><input type="text" name="noet"></td></tr>
+        <tr><td>Année scolaire:</td><td><input type="text" name="annee"></td></tr>
+        <tr><td>Filière:</td><td><input type="text" name="filiere"></td></tr>
+        <tr><td><input type="submit" name="Ajouter"></td></tr>
+    </table>
 </form>
-
