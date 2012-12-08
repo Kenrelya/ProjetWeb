01dc26d2dfb7d7a2c2b31ae5ddc41a865e704d7f
ldestin <ldestin@172.16.250.81>
2012-12-08 23:34:46 +0100

    Test

--- projet/vues/scolarite/affectgrpmod.php
+++ projet/vues/scolarite/affectgrpmod.php
@@ -1,39 +1,16 @@
 <form action="affectation2.php" method="post">
 <select name="gid">
 
-<?php
-$SQL="SELECT * FROM groupes";
-$res=mysql_query($SQL);
-while ($row=mysql_fetch_array($res))
-{
-  
-  echo "<option value=$row[gid]>$row[intitule]</option>";
-}
-?>
+<?php while ($row=mysql_fetch_array($groupes)){ ?>
+    <option value=$row[gid]>$row[intitule]</option>
+<?php }?>
+
 </select>
 <select name="mid">
 
-<?php
-$SQL1="SELECT * FROM modules";
-$res1=mysql_query($SQL1);
-while ($row1=mysql_fetch_array($res1))
-{
-  
-  echo "<option value=$row1[mid]>$row1[discipline]</option>";
-}
-?>
-</select>
-    <select name="intitule">
-
-<?php
-$SQL2="SELECT * FROM modules";
-$res2=mysql_query($SQL1);
-while ($row2=mysql_fetch_array($res2))
-{
-  
-  echo "<option value=$row2[mid]>$row2[intitule]</option>";
-}
-?>
+<?php while ($row1=mysql_fetch_array($modules)){ ?>
+    <option value=<?php $row1[mid]; ?>>$row1[intitule]</option>
+<?php }?>
 </select>
 <input type="submit" value="envoyer">
 </form>
